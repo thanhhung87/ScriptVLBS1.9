feaf3fb0a08d90715b02feac3420946cb2ed3980
hungnt <thanhhung.vip@gmail.com>
2026-01-09 09:17:28 +0700

    Update v1.2 setdo vukhi.lua for CBR and CLD

--- Scripts/LIB/setdo/vukhi.lua
+++ Scripts/LIB/setdo/vukhi.lua
@@ -3,16 +3,22 @@ ThemSetDoByType({
 	[139] = 1, -- ky nang vong co
 	[116] = 20, -- toc do danh (noi cong)
 })
-
--- -- Vi du su dung ThemSetDoByType - Chi loc kiem co hut noi luc
--- ThemSetDoByType({
--- 	itemType = tbType.VuKhi,  -- Chi loc vk
--- 	[171] = 100, -- loi sat noi cong
---     [116] = 20, -- toc do danh (noi cong)
--- })
+-- VD quyen
+ThemSetDoByType({
+	itemType = tbType.VuKhi,  -- Chi loc vk
+	[171] = 180, -- loi sat noi cong
+    [116] = 20, -- toc do danh (noi cong)
+	[108] = 40, -- tg trung doc
+})
+-- CBR
+ThemSetDoByType({
+	itemType = tbType.VuKhi,  -- Chi loc vk
+	[170] = 180, -- hoa sat noi cong
+    [116] = 20, -- toc do danh (noi cong)
+})
+-- CLD
 ThemSetDoByType({
 	itemType = tbType.VuKhi.Dao,  -- Chi loc vk dao
     [115] = 30, -- toc do danh (ngoai cong)
 	[125] = 30, -- doc sat ngoai cong
-
 })
