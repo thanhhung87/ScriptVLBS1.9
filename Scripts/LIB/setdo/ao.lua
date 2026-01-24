eed4cd41cfc6400a71e5248d7512518d9ea2416f
hungnt <thanhhung.vip@gmail.com>
2026-01-24 11:41:46 +0700

    feat item Nam Nu

--- Scripts/LIB/setdo/ao.lua
+++ Scripts/LIB/setdo/ao.lua
@@ -1,5 +1,24 @@
--- ThemSetDoByType({
--- 	itemType = tbType.Ao.TatCa,
--- 	[113] = 40, -- thoi gian phuc hoi
---     [104] = 25, -- ptvl
--- })
+--thuy
+ThemSetDoByType({
+	itemType = tbType.Ao.TatCa,
+	-- [113] = 40, -- thoi gian phuc hoi
+    [102] = 10, -- khang hoa
+})
+--Moc -- Nam
+ThemSetDoByType({
+	itemType = tbType.Ao.Nam,
+	-- [113] = 40, -- thoi gian phuc hoi
+    [103] = 10, -- khang loi
+})
+--Hoa - nam
+ThemSetDoByType({
+	itemType = tbType.Ao.Nam,
+	-- [113] = 40, -- thoi gian phuc hoi
+    [104] = 10, -- ptvl
+})
+--Kim - Nu
+ThemSetDoByType({
+	itemType = tbType.Ao.Nu,
+	-- [113] = 40, -- thoi gian phuc hoi
+    [101] = 10, -- khang doc
+})
--- Scripts/LIB/setdo/giay.lua
+++ Scripts/LIB/setdo/giay.lua
@@ -1,13 +1,35 @@
+
+-- ThemSetDoByType({
+-- 	itemType = tbType.Giay.TatCa,
+--     [111] = 20, -- toc do di chuyen
+-- 	-- [102] = 25, -- khang hoa
+--     -- [110] = 40, -- tg lam choang
+-- })
+--kim
+ThemSetDoByType({
+	itemType = tbType.Giay.Nam,
+    [111] = 20, -- toc do di chuyen
+	[101] = 10, -- khang doc
+})
+--Hoa
 ThemSetDoByType({
 	itemType = tbType.Giay.TatCa,
-    [111] = 40, -- toc do di chuyen
-	[104] = 25, -- ptvl
-    [106] = 40, -- tg lam cham
+    [111] = 20, -- toc do di chuyen
+	[104] = 10, -- ptvl
 })
+--Tho -- nam
 ThemSetDoByType({
-	itemType = tbType.Giay.TatCa,
-    [111] = 40, -- toc do di chuyen
-	[102] = 25, -- khang hoa
-    [110] = 40, -- tg lam choang
+	itemType = tbType.Giay.Nam,
+    [111] = 20, -- toc do di chuyen
+	[105] = 10, -- khang bang
 })
+-- Moc -- nu
+ThemSetDoByType({
+	itemType = tbType.Giay.Nu,
+    [111] = 20, -- toc do di chuyen
+	[103] = 10, -- khang loi
+})
+
+
+
 
